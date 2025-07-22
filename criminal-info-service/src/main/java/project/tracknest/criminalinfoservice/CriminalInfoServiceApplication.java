ab82fb8fc2390b0f55cd3bd6df5d4687ffd5bcd5
NguyenVu04 <nguyenvu04.work@gmail.com>
2025-07-22 22:19:17 +0700

    init

---
+++ criminal-info-service/src/main/java/project/tracknest/criminalinfoservice/CriminalInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package project.tracknest.criminalinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriminalInfoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriminalInfoServiceApplication.class, args);
+    }
+
+}
---
+++ criminal-info-service/src/test/java/project/tracknest/criminalinfoservice/CriminalInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.tracknest.criminalinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriminalInfoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
