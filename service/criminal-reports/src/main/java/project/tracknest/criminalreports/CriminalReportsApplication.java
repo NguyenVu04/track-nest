f2e53c7cbe1d33298019e016772de298d1961784
NguyenVu04 <nguyenvu04.work@gmail.com>
2025-09-12 01:29:11 +0700

    init iam

---
+++ service/criminal-reports/src/main/java/project/tracknest/criminalreports/CriminalReportsApplication.java
@@ -0,0 +1,13 @@
+package project.tracknest.criminalreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriminalReportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriminalReportsApplication.class, args);
+    }
+
+}
---
+++ service/criminal-reports/src/test/java/project/tracknest/criminalreports/CriminalReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.tracknest.criminalreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriminalReportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
