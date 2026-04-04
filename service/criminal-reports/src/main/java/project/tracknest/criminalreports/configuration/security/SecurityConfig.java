55cb763d7e9a43736eced19eb1467c3b81d61dfe
NguyenVu04 <nguyenvu04.work@gmail.com>
2026-04-04 23:36:21 +0700

    add cors

--- service/criminal-reports/src/main/java/project/tracknest/criminalreports/configuration/security/SecurityConfig.java
+++ service/criminal-reports/src/main/java/project/tracknest/criminalreports/configuration/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -26,6 +27,8 @@ public class SecurityConfig {
         http
                 .addFilterAfter(new KeycloakFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**")
+                        .permitAll()
                         .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/api-docs/**")
                         .permitAll()
                         .requestMatchers("/actuator/**")
