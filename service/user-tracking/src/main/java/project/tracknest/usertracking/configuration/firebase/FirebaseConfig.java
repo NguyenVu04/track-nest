c04c5a7e6054901eee96cfe8d645e1b658f8c931
NguyenVu04 <nguyenvu04.work@gmail.com>
2026-04-26 17:34:57 +0700

    add cd

--- service/user-tracking/src/main/java/project/tracknest/usertracking/configuration/firebase/FirebaseConfig.java
+++ service/user-tracking/src/main/java/project/tracknest/usertracking/configuration/firebase/FirebaseConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -14,8 +15,10 @@ import java.io.InputStream;
 public class FirebaseConfig {
     @PostConstruct
     public void init() throws IOException {
-        InputStream serviceAccount =
-                new ClassPathResource("firebase-service-account.json").getInputStream();
+        String path = System.getenv("FIREBASE_SERVICE_ACCOUNT_PATH");
+        InputStream serviceAccount = (path != null && !path.isBlank())
+                ? new FileInputStream(path)
+                : new ClassPathResource("firebase-service-account.json").getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
